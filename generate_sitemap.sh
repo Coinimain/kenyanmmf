57e2706de4fd42340cb2365d4bc412637170493a
GitHub Action <action@github.com>
2025-08-20 00:15:04 +0300

    Update sitemaps with auto-generation and index

--- generate_sitemap.sh
+++ generate_sitemap.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-# Generate sitemap.xml with current date
+# Generate sitemap.xml and sitemap.xml.gz with current date
+# Then generate sitemap_index.xml that lists both
 
 today=$(date +"%Y-%m-%d")
 
+# Start sitemap.xml
 cat > sitemap.xml <<EOL
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -12,23 +14,35 @@ cat > sitemap.xml <<EOL
     <changefreq>weekly</changefreq>
     <priority>1.0</priority>
   </url>
-  <url>
-    <loc>https://kenyammfcalculator.co.ke/about.html</loc>
+EOL
+
+# Loop through all .html files in current folder
+for file in *.html; do
+  echo "  <url>" >> sitemap.xml
+  echo "    <loc>https://kenyammfcalculator.co.ke/$file</loc>" >> sitemap.xml
+  echo "    <lastmod>$today</lastmod>" >> sitemap.xml
+  echo "    <changefreq>monthly</changefreq>" >> sitemap.xml
+  echo "    <priority>0.8</priority>" >> sitemap.xml
+  echo "  </url>" >> sitemap.xml
+done
+
+# Close sitemap.xml
+echo "</urlset>" >> sitemap.xml
+
+# Create compressed sitemap
+gzip -c sitemap.xml > sitemap.xml.gz
+
+# Generate sitemap index
+cat > sitemap_index.xml <<EOL
+<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <sitemap>
+    <loc>https://kenyammfcalculator.co.ke/sitemap.xml</loc>
     <lastmod>$today</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>https://kenyammfcalculator.co.ke/contact.html</loc>
+  </sitemap>
+  <sitemap>
+    <loc>https://kenyammfcalculator.co.ke/sitemap.xml.gz</loc>
     <lastmod>$today</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>https://kenyammfcalculator.co.ke/faq.html</loc>
-    <lastmod>$today</lastmod>
-    <changefreq>monthly</changefreq>
-    <priority>0.8</priority>
-  </url>
-</urlset>
+  </sitemap>
+</sitemapindex>
 EOL
